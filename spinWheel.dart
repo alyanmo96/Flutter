1dd850f066656de3f70f80cc0010a0ffb7fab78f
Mohamed Alian <50428010+alyanmo96@users.noreply.github.com>
2021-06-04 16:15:06 +0300

    Add files via upload

---
+++ spinWheel.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinning_wheel/flutter_spinning_wheel.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SpiningWheel(),
+    );
+  }
+}
+
+class SpiningWheel extends StatefulWidget {
+  @override
+  _SpiningWheelState createState() => _SpiningWheelState();
+}
+
+class _SpiningWheelState extends State<SpiningWheel> {
+  final StreamController _dividerController = StreamController<int>();
+  final _wheelNotifier = StreamController<double>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _dividerController.close();
+    _wheelNotifier.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color(0xffDDC3FF), elevation: 0.0),
+      backgroundColor: Color(0xffDDC3FF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinningWheel(
+              Image.asset('assets/images/roulette-8-300.png'),
+              width: 310,
+              height: 310,
+              initialSpinAngle: _generateRandomAngle(),
+              spinResistance: 0.6,
+              canInteractWhileSpinning: false,
+              dividers: 8,
+              onUpdate: _dividerController.add,
+              onEnd: _dividerController.add,
+              secondaryImage:
+                  Image.asset('assets/images/roulette-center-300.png'),
+              secondaryImageHeight: 110,
+              secondaryImageWidth: 110,
+              shouldStartOrStop: _wheelNotifier.stream,
+            ),
+            SizedBox(height: 30),
+            StreamBuilder(
+              stream: _dividerController.stream,
+              builder: (context, snapshot) =>
+                  snapshot.hasData ? RouletteScore(snapshot.data) : Container(),
+            ),
+            SizedBox(height: 30),
+            new ElevatedButton(
+              child: new Text("Start"),
+              onPressed: () =>
+                  _wheelNotifier.sink.add(_generateRandomVelocity()),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  double _generateRandomVelocity() => (Random().nextDouble() * 6000) + 2000;
+
+  double _generateRandomAngle() => Random().nextDouble() * pi * 2;
+}
+
+
+
+
+class RouletteScore extends StatelessWidget {
+  final int selected;
+
+  final Map<int, String> labels = {
+    1: '1000\$',
+    2: '400\$',
+    3: '800\$',
+    4: '7000\$',
+    5: '5000\$',
+    6: '300\$',
+    7: '2000\$',
+    8: '100\$',
+  };
+
+  RouletteScore(this.selected);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${labels[selected]}',
+        style: TextStyle(fontStyle: FontStyle.italic, fontSize: 24.0));
+  }
+}
